dd75eb1dcc05942bf1ce1454d3d0aa4a6cb69ac0
Aharon Fajardo <Aharon.Fajardo@arrowdigital.com>
2019-07-18 10:09:41 -0400

    replaced getByClassName calls with querySelectorAll calls, and replaced filter calls with find calls, on day 1

--- 01-JavaScript_Drum_Kit/script.js
+++ 01-JavaScript_Drum_Kit/script.js
@@ -5,20 +5,20 @@ document.querySelector("body").addEventListener("keyup", unhighlight);
 
 function highlight(e) {
   let keyCode = e.which;
-  let instruments = [...document.getElementsByClassName("key")];
-  let activeInstr = instruments.filter(instrument => instrument.getAttribute("data-key") == keyCode)[0];
+  let instruments = [...document.querySelectorAll("div.key")];
+  let activeInstr = instruments.find(instrument => instrument.getAttribute("data-key") == keyCode);
   if (activeInstr) activeInstr.classList.add("playing");
 }
 function unhighlight(e) {
   let keyCode = e.which;
-  let instruments = [...document.getElementsByClassName("key")];
-  let activeInstr = instruments.filter(instrument => instrument.getAttribute("data-key") == keyCode)[0];
+  let instruments = [...document.querySelectorAll("div.key")];
+  let activeInstr = instruments.find(instrument => instrument.getAttribute("data-key") == keyCode);
   if (activeInstr) activeInstr.classList.remove("playing");
 }
 function playSound(e) {
   let keyCode = e.which;
-  let instruments = [...document.getElementsByTagName("AUDIO")];
-  let activeInstr = instruments.filter(instrument => instrument.getAttribute("data-key") == keyCode)[0];
+  let instruments = [...document.querySelectorAll("audio")];
+  let activeInstr = instruments.find(instrument => instrument.getAttribute("data-key") == keyCode);
   if (activeInstr) {
     activeInstr.play();
     activeInstr.currentTime = 0;
