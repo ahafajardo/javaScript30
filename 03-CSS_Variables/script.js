882bda889ce8f9f847f3a3db9b269a812c2f6891
Aharon Fajardo <Aharon.Fajardo@arrowdigital.com>
2019-07-19 15:09:36 -0400

    achieved functionality in day 4

--- 03-CSS_Variables/script.js
+++ 03-CSS_Variables/script.js
@@ -2,7 +2,7 @@ let root = document.documentElement;
 
 const inputs = [...document.querySelectorAll(".controls input")];
 
-inputs.map(input => input.addEventListener("input", updateElements));
+inputs.forEach(input => input.addEventListener("input", updateElements));
 
 function updateElements() {
   root.style.setProperty(`--${this.name}`, `${this.value}${this.dataset.sizing || ""}`);
