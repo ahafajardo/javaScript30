ab23e81807f0e2d4371d1b47b11ae64af783f31f
Aharon Fajardo <Aharon.Fajardo@arrowdigital.com>
2019-07-23 11:22:27 -0400

    achieved functionality in day 10

---
+++ 10-Hold_Shift_and_Check_Checkboxes/script.js
@@ -0,0 +1,25 @@
+const checkboxes = document.querySelectorAll(".item input");
+
+let checkedIndex = 0;
+let lastChecked = checkboxes[0];
+let lastCheckedIndex = 0;
+let shiftHeld = false;
+
+document.getRootNode().addEventListener("keydown", e => {
+  shiftHeld = e.which == 16 ? true : shiftHeld;
+});
+document.getRootNode().addEventListener("keyup", e => (shiftHeld = e.which == 16 ? false : shiftHeld));
+
+checkboxes.forEach(checkbox =>
+  checkbox.addEventListener("click", e => {
+    if (shiftHeld) {
+      checkedIndex = [...checkboxes].indexOf(e.target);
+      let indexRange = Math.abs(checkedIndex - lastCheckedIndex);
+      let startIndex = checkedIndex > lastCheckedIndex ? lastCheckedIndex : checkedIndex;
+      let toCheck = [...checkboxes].slice(startIndex + 1, startIndex + indexRange);
+      toCheck.forEach(checkbox => (checkbox.checked = !checkbox.checked));
+    }
+    lastChecked = e.target;
+    lastCheckedIndex = [...checkboxes].indexOf(lastChecked);
+  })
+);
